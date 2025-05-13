f2b68a8138ccef8c4e2e055a6dce923baea97950
Andrew Jorczak <andrew.jorczak@gmail.com>
2025-05-13 15:10:18 -0700

    Boot and migration set up.

--- db/migrate/20250513193500_create_addresses.rb
+++ db/migrate/20250513193500_create_addresses.rb
@@ -1,6 +1,10 @@
 class CreateAddresses < ActiveRecord::Migration[8.0]
   def change
-    create_table :addresses do |t|
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :addresses, id: :uuid do |t|
+      t.string "primary_line"
+      t.string "zip_code", null: false
+      t.string "state", null: false
       t.timestamps
     end
   end
